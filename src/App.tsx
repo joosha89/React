92cf5dac46b1ac19c933f9998d1221ed8938ebbf
joosha89 <joosha89@gmail.com>
2021-03-22 16:17:10 +0900

    중간커밋

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,31 @@
 import React, { useState } from 'react';
-import Data from './data';
 import { Link, Route, Switch } from 'react-router-dom';
+import { Button,  Navbar,  Nav,  Jumbotron,  Form,  FormControl} from 'react-bootstrap';
 import Detail from './Detail';
-import {
-  Button,
-  Navbar,
-  Nav,
-  Jumbotron,
-  Form,
-  FormControl,
-} from 'react-bootstrap';
+import Cart from './Cart';
+import Data from './data';
+
 
 //import './App.css';
 /* import {Button, Progress} from 'semantic-ui-react'; */
 
-function App() {
+const App: React.FunctionComponent = () => {
   let [shoes, shoesState] = useState(Data);
-
-  /* function conLog() {
-        console.log(shoes);
-    } */
+  let [cartItem, cartItemState] = useState(0);
 
   return (
     <>
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="/">HOME</Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="/cart">CART</Nav.Link>
+          <Link to="/cart">CART</Link>
         </Nav>
         <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
           <Button variant="outline-info">Search</Button>
         </Form>
       </Navbar>
+
       <Switch>
         <Route exact path="/">
           <Jumbotron></Jumbotron>
@@ -40,7 +33,7 @@ function App() {
             {shoes.map((num, i) => {
               return (
                 <Link to={'/detail/' + i}>
-                  <Card shoes={num} i={i} key={i} />;
+                  <Item shoes={num} i={i} key={i} />;
                 </Link>
               );
             })}
@@ -50,15 +43,19 @@ function App() {
         <Route path="/detail/:id">
           <Detail shoes={shoes}></Detail>
         </Route>
+
+        <Route path="/cart/">
+          {/* <Cart></Cart> */}
+        </Route>
       </Switch>
     </>
   );
 }
 
-function Card(props) {
+function Item(props: any) {
   return (
     <div className="item" style={{ display: 'inline-block', width: '40vh' }}>
-      <img src={props.shoes.img} style={{ width: '100%' }} />
+      <img src={props.shoes.img} style={{ width: '100%' }} alt=""/>
       <h4>{props.shoes.title}</h4>
       <p>
         {props.shoes.content} & {props.shoes.price}
@@ -67,14 +64,5 @@ function Card(props) {
   );
 }
 
-/* function Modal() {
-    return (
-        <div className="modal">
-            <h2>wpahr</h2>
-            <p>date</p>
-            <p>상세내용</p>
-        </div>
-    );
-} */
 
 export default App;
--- src/Detail.js
+++ src/Detail.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { useHistory, useParams } from 'react-router-dom';
+import { Cart } from './Cart';
 
-function Detail(props) {
+const Detail = (props) => {
   let history = useHistory();
   let { id } = useParams();
   let searchItem = props.shoes.find((item) => {
     return item.id == id;
   });
+
+  const setCart = () => {
+
+  }
 
   return (
     <div className="container">
@@ -19,22 +24,8 @@ function Detail(props) {
             <p>{searchItem.content}</p>
             <p>{searchItem.price}</p>
             <button className="btn btn-danger">주문하기</button>
-            <button
-              onClick={() => {
-                history.goBack();
-              }}
-              className="btn btn-danger"
-            >
-              뒤로가기
-            </button>
-            <button
-              onClick={() => {
-                history.push('/');
-              }}
-              className="btn btn-danger"
-            >
-              뒤로가기
-            </button>
+            <button onClick={() => {setCart();} } className="btn btn-warning">카트담기</button>
+            <button onClick={() => { history.push('/'); }} className="btn btn-danger">뒤로가기</button>
           </div>
         </div>
       </div>
