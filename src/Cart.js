0fb8cfa1d636a876d4f2fe0a6573b4b646c5acd6
joosha89 <joosha89@nmp-corp.com>
2021-03-24 16:01:36 +0900

    장바구니 추가

--- src/Cart.js
+++ src/Cart.js
@@ -1,3 +1,13 @@
-const Cart = (props) => {
+const defaultState = {
+    items: JSON.parse(localStorage.getItem("items")) || []
+};
+
+const Cart = (state = defaultState) => {
+    return (
+        <>
+            console.log(items);
+        </>
+    );
+
 }
 export default Cart;
--- src/Detail.js
+++ src/Detail.js
@@ -10,7 +10,7 @@ const Detail = (props) => {
   });
 
   const setCart = () => {
-
+    localStorage.setItem();
   }
 
   return (
